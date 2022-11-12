7af57ad589bcccda67171c3806faab9da00daa4e
xincheng zhou <prifvibea@gmail.com>
2022-11-12 13:09:08 -0600

    change port

--- database/index.js
+++ database/index.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'ec2-18-188-60-79.us-east-2.compute.amazonaws.com',
   port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '12341234',
-  database: process.env.DB_NAME || 'SDC'
+  user: process.env.DB_USER || 'ubuntu',
+  password: process.env.DB_PASSWORD || 'ubuntu',
+  database: process.env.DB_NAME || 'sdc'
 });
 
 pool.on('error', (err, client) => {
---
+++ script.js
@@ -0,0 +1,40 @@
+import http from 'k6/http';
+import { check,group,sleep } from 'k6';
+
+export const options = {
+  vus:50,
+  duration:'30s'
+}
+
+const route = `http://localhost:3000/reviews/`
+
+export default function test() {
+  group('getAllReviews', () => {
+    const getAllReviews = http.get(route+'?product_id=66648');
+    check(getAllReviews, {
+      'transaction time < 10ms': (r) => r.timings.duration < 10,
+      'transaction time < 50ms': (r) => r.timings.duration < 50,
+      'transaction time < 200ms': (r) => r.timings.duration < 200,
+      'transaction time < 500ms': (r) => r.timings.duration < 500,
+      'transaction time < 1000ms': (r) => r.timings.duration < 1000,
+      'transaction time < 2000ms': (r) => r.timings.duration < 2000,
+      'transaction time < 5000ms': (r) => r.timings.duration < 5000,
+      'transaction time < 10s': (r) => r.timings.duration < 10000,
+      'transaction time < 20s': (r) => r.timings.duration < 20000,
+    });
+  })
+  group('getMeta', () => {
+    const getAllReviews = http.get(route+'meta/?product_id=66648');
+    check(getAllReviews, {
+      'transaction time < 10ms': (r) => r.timings.duration < 10,
+      'transaction time < 50ms': (r) => r.timings.duration < 50,
+      'transaction time < 200ms': (r) => r.timings.duration < 200,
+      'transaction time < 500ms': (r) => r.timings.duration < 500,
+      'transaction time < 1000ms': (r) => r.timings.duration < 1000,
+      'transaction time < 2000ms': (r) => r.timings.duration < 2000,
+      'transaction time < 5000ms': (r) => r.timings.duration < 5000,
+      'transaction time < 10s': (r) => r.timings.duration < 10000,
+      'transaction time < 20s': (r) => r.timings.duration < 20000,
+    });
+  })
+};
